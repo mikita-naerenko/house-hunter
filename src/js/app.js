86334a4ccf028fc6d0794729634fa2b7e955aa4f
Mikita <tetropak555666@gmail.com>
2023-02-25 19:43:00 +0900

    implemented functions  to render and modal show photo room-tour section

--- src/js/app.js
+++ src/js/app.js
@@ -42,7 +42,6 @@ const renderProductCard = function(productCardArr) {
 const renderArticles = function(articlesArr) {
     const firstThreeArticles = articlesArr.sort((a,b) => a.date.getTime() - b.date.getTime()).slice(0,3);
     const [mainArticle] = articlesArr.slice(-1);
-    console.log(mainArticle);
     firstThreeArticles.forEach(article => {
         const html = `<li class="articles-list__item">
                         <img class="articles-list__item-image" srcset="${article.img}" width="100" height="75" alt="">
@@ -76,8 +75,43 @@ const renderArticles = function(articlesArr) {
                             <p class="articles-preview__date"><span>${mainArticle.readTime} read</span><time datetime="${mainArticle.date.toISOString()}">${new Intl.DateTimeFormat('en-US', {month: "long", day: "numeric", year: "numeric"}).format(mainArticle.date)}</time></p>
                         </div>
                     </li>`
-                    document.querySelector('.articles-list--main').insertAdjacentHTML('afterbegin', htmlMainArticle);
-}
+        document.querySelector('.articles-list--main').insertAdjacentHTML('afterbegin', htmlMainArticle);
+};
+
+const renderRoomTourSection = function(roomTourArr) {
+    let currentTour = 0;
+    const optionsMap = new Map(Object.entries(roomTourArr[currentTour].houseOptions));
+    optionsMap.forEach((value, key) => {
+        
+        const html = `<li class="room-tour-description__detail-item room-tour-description__detail-item--${key}">${value} ${key}</li>`;
+        document.querySelector('.room-tour-description__detail-list').insertAdjacentHTML('afterbegin', html);
+    });
+
+    const htmlSaller = `<div class="saller saller--room-tour">
+                    <picture><img srcset="${roomTourArr[currentTour].saller.avatar}"  width="40" alt=""></picture>
+                    <div class="slide__wrapper">
+                    <a href="#" class="saller__name">${roomTourArr[currentTour].saller.name}</a>
+                    <a href="#" class="saller__text">${roomTourArr[currentTour].saller.position}</a>
+                    </div>
+                    <a class="main-button main-button__contact" href="tel:">
+                        <svg class="main-button--phone" width="14" height="14" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M8.38439 2.47912C8.90949 2.3208 9.47298 2.34711 9.98104 2.55365C10.4891 2.7602 10.9111 3.13453 11.1768 3.61432L11.2668 3.79312L12.06 5.55832C12.3011 6.09411 12.3793 6.68894 12.285 7.26885C12.1907 7.84876 11.928 8.38814 11.5296 8.81992L11.3712 8.97832L10.1184 10.1459C9.8928 10.3595 10.062 11.1863 10.8792 12.5999C11.6124 13.8719 12.2112 14.4659 12.504 14.4983H12.5556L12.6192 14.4863L15.0792 13.7339C15.4098 13.6325 15.7626 13.6285 16.0955 13.7224C16.4283 13.8162 16.727 14.004 16.956 14.2631L17.0664 14.4011L18.6936 16.6571C19.0129 17.0992 19.1721 17.6368 19.1451 18.1814C19.118 18.7261 18.9063 19.2453 18.5448 19.6535L18.3984 19.8047L17.7468 20.4215C17.1627 20.9746 16.4322 21.3484 15.6419 21.4984C14.8516 21.6483 14.0349 21.5682 13.2888 21.2675C10.9668 20.3315 8.85719 18.1931 6.94079 14.8739C5.02079 11.5463 4.2228 8.64592 4.5792 6.16192C4.68715 5.41091 4.99653 4.70316 5.47446 4.11388C5.9524 3.52459 6.58104 3.07578 7.2936 2.81512L7.52519 2.73832L8.38439 2.47912Z" fill="white"/>
+                            </svg>
+                        <span>Contact Now</span>
+                        </a>
+                    </div>`;
+    document.querySelector('.room-tour-description').insertAdjacentHTML('beforeend', htmlSaller);
+    
+    const htmlMainPhoto = `<img srcset="${roomTourArr[currentTour].photoList.slice(0, 1)}" width="60" height="60" alt="" class="show-photo__item show-photo__item--main">`;
+    document.querySelector('.show-photo').insertAdjacentHTML('afterbegin', htmlMainPhoto);
+    roomTourArr[currentTour].photoList.slice(1, 4).forEach((img, i) => {
+        let html;
+        i === 0 ? html = `<img srcset="${img}" width="60" height="60" alt="" class="show-photo__item show-photo__item--next">`
+        : html = `<img srcset="${img}" width="60" height="60" alt="" class="show-photo__item"></img>`;
+        document.querySelector('.show-photo__wrapper').insertAdjacentHTML('beforeend', html);
+    });
+
+};
 
 const typeOfProductButtonHandler = function(productCardArr, e) {
     const target = e.target.getAttribute('id');
@@ -86,13 +120,28 @@ const typeOfProductButtonHandler = function(productCardArr, e) {
     while (slideContainer.firstChild)
     slideContainer.removeChild(slideContainer.firstChild);
     renderProductCard(productCardArr.filter(el => el.category.type === target));
-}
+};
+
+const showPhotoOnClickHandler = function(e){
+    const target = e.target.getAttribute('srcset');
+    const modalPhoto = document.querySelector('.big-photo');
+    const closeModalFunctionKeydown = function(e) {
+        if (e.key === 'Escape') modalPhoto.classList.add('hidden');
+        window.removeEventListener('keydown', closeModalFunctionKeydown);
+    };
+    const closeModalFunction = function(e) {
+        modalPhoto.classList.add('hidden');
+        document.querySelector('.big-photo__close').removeEventListener('click', closeModalFunction);
+    };
+    if (!target) return;
+    modalPhoto.classList.remove('hidden');
+    modalPhoto.querySelector('img').srcset = target;
+    document.querySelector('.big-photo__close').addEventListener('click', closeModalFunction);
+    window.addEventListener('keydown', closeModalFunctionKeydown);
+    
+};
 
  getUserData().then(data =>{
-    
-    sallerRoomTour.querySelector('img').srcset = data[0].userData[0].avatar;
-    sallerRoomTour.querySelector('.saller__name').textContent = data[0].userData[0].name;
-    sallerRoomTour.querySelector('.saller__text').textContent = data[0].userData[0].position;
 
     const productCardArr = data[1].productCard;
     const userArr = data[0].userData;
@@ -110,10 +159,18 @@ const typeOfProductButtonHandler = function(productCardArr, e) {
             if (user.id === article.authorId) article.author = user;
         });
     });
-    // console.log(articlesArr);
+    const roomTourArr = data[3].roomTour;
+    roomTourArr.forEach(item => {
+        sallerArr.map(saller => {
+            if (saller.id === item.sallerId) item.saller = saller;
+        });
+    });
+    
     renderArticles(articlesArr);
     renderProductCard(productCardArr);
+    renderRoomTourSection(roomTourArr);
     document.querySelector('.featured-house__input-list').addEventListener('click', typeOfProductButtonHandler.bind(null, productCardArr));
+    document.querySelector('.show-photo').addEventListener('click', showPhotoOnClickHandler)
 });
 
 
